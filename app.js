86624ae25ab335a62607dcd47a1d3f6ffcbf78b0
deepaksaroha <deepaksaroha1993@gmail.com>
2021-06-29 12:51:04 +0530

    all files

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const shortid = require('shortid');
+
+urls = {}
+
+app.use('/', express.static('static'));
+
+app.use(express.json());
+
+app.get('/u/:shortUrlId', (req, res, next)=>{
+    const longUrl = urls[req.params.shortUrlId];
+    if (longUrl){
+        res.redirect(longUrl);
+    }else{
+        res.status(400).send("Invalid URL");
+    }
+});
+
+app.post('/url', (req, res, next)=>{
+    const shortUrlId = shortid.generate();
+    urls[shortUrlId] = req.body.longUrl;
+    res.send({
+        shortUrl: `http://localhost:3000/u/${shortUrlId}`
+    });
+})
+
+app.get('/urls', (req, res, next)=>{
+    res.send(urls);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at port: ${port}`)
+});
---
+++ static/js/index.js
@@ -0,0 +1,60 @@
+window.onload = ()=> {
+
+    fetch('http://localhost:3000/urls')
+    .then(res=>{
+        return res.json();
+    }).then(res=>{
+        document.querySelector("#dashboard").innerText = '';
+    })
+
+    const btn1 = document.querySelector("#btn");
+    btn1.addEventListener("click", (event)=>{
+        event.preventDefault();
+        const formele = document.querySelector("#form");
+        const formData = new FormData(formele);
+        if(validate(formData.get('longUrl'))){
+            const request = new Request('http://localhost:3000/url', {
+                method: 'POST',
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                }),
+                body: JSON.stringify(Object.fromEntries(formData))
+            });
+            fetch(request).then((res)=>{
+                return res.json();
+            }).then((res)=>{
+                document.querySelector("#output").innerText = res.shortUrl;
+            });
+        }else{
+            document.querySelector("#output").innerText = "Invalid Url";
+        }
+
+        fetch('http://localhost:3000/urls')
+        .then(res=>{
+            return res.json();
+        }).then(res=>{
+            let stg = '';
+            for(const key in res){
+                stg += 'http://localhost:3000/' + key + ' : ' + res[key] + '\n'
+            }
+            document.querySelector("#dashboard").innerText = stg;
+        })
+    });
+
+    const cpy = document.querySelector("#copy");
+    cpy.addEventListener("click", ()=>{
+        const copytext = document.querySelector("#output");
+        copytext.select();
+        document.execCommand("copy");
+    })
+
+    function validate(url){
+        var patt = /(https|http):\/\/\S+\.\S+/;
+        var result = url.match(patt);
+        if(result){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}

b7f88c2e83a654d5441c383a4c2be024328c5311
deepaksaroha <deepaksaroha1993@gmail.com>
2021-06-29 13:15:18 +0530

    changed wrong urls

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post('/url', (req, res, next)=>{
     const shortUrlId = shortid.generate();
     urls[shortUrlId] = req.body.longUrl;
     res.send({
-        shortUrl: `http://url-shortner-exp/u/${shortUrlId}`
+        shortUrl: `http://url-shortner-exp.herokuapp.com/u/${shortUrlId}`
     });
 })
 
